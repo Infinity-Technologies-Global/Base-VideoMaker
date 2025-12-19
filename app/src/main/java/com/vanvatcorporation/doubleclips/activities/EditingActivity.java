750716971c0a4ef1b9afd51de5c4b7856a0a10f8
Troll <viet2007ht@gmail.com>
2025-12-19 22:19:41 +0700

    idk asking her to go out i guess afsladfj worst she can say is no so why not

--- app/src/main/java/com/vanvatcorporation/doubleclips/activities/EditingActivity.java
+++ app/src/main/java/com/vanvatcorporation/doubleclips/activities/EditingActivity.java
@@ -1211,6 +1211,7 @@ public class EditingActivity extends AppCompatActivityImpl {
 
         // TODO: Tested for dragging back and forth clips. They're doing fine with the extractor SYNC_EXACT
         //  Limit the time of refreshing entire timeline like this.
+        //  adfadfsdfsdf worst she can say ís no
         timelineRenderer.buildTimeline(timeline, properties, settings, this, previewViewGroup, textCanvasControllerInfo);
     }
     private void setCurrentTime(float value)
